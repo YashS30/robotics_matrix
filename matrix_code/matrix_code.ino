16099b9cf316d3a48e0a129b955d92938e013f9c
Steven Mu <xsmu0922@gmail.com>
2021-08-24 16:13:38 -0400

    Create matrix_code.ino

---
+++ matrix_code/matrix_code.ino
@@ -0,0 +1,15 @@
+#include <FastLED.h>
+
+#define LED_TYPE WS2812
+#define NUM_LEDS 9
+#define DATA_PIN 12
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
